46b8e8b6327e9b853ac7835930ee8a4e99fb6537
Ricardo <ricardocabral.net@gmail.com>
2019-04-29 21:54:30 -0300

    commit first

---
+++ Invest/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Invest
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
